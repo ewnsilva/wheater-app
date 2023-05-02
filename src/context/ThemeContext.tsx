195777e4408fedab395603a513ccdd2addc34158
Ericles Willian <ewnsilva@gmail.com>
2023-05-01 21:24:35 -0300

    style: Atualizando formatação de componente.

--- src/context/ThemeContext.tsx
+++ src/context/ThemeContext.tsx
@@ -1,42 +1,38 @@
-import { createTheme } from '@mui/material';
-import { red, grey } from '@mui/material/colors'
+import { createTheme } from "@mui/material";
+import { red, grey } from "@mui/material/colors";
 
-const defaultTheme = createTheme({
-    palette:{
-        primary:{
-            main: red[400],
-            dark: red[600],
-            light: red[200],
-            contrastText: '#ffffff'
-        },
-        secondary: {
-            main: grey[500],
-            dark: grey[700],
-            light: grey[300],
-            contrastText: '#ffffff'
-        },
-        background: {
-            paper: '#ffffff',
-            default: '#f7f6f3'
-        }
+export const defaultTheme = createTheme({
+  palette: {
+    primary: {
+      main: red[400],
+      dark: red[600],
+      light: red[200],
+      contrastText: "#ffffff",
     },
-    typography:{
-        fontFamily: 'arial',
-        h2:{
-            color: '#ffffff',
-            alignItems: 'center',
-            justifyItems: 'center',
-            alignContent: 'center',
-
-        },
-        body1:{
-            color: 'black',
-        },
-        body2:{
-            color: '#ffffff',
-        }
+    secondary: {
+      main: grey[500],
+      dark: grey[700],
+      light: grey[300],
+      contrastText: "#ffffff",
     },
-
-})
-
-export default defaultTheme
+    background: {
+      paper: "#ffffff",
+      default: "#f7f6f3",
+    },
+  },
+  typography: {
+    fontFamily: "arial",
+    h2: {
+      color: "#ffffff",
+      alignItems: "center",
+      justifyItems: "center",
+      alignContent: "center",
+    },
+    body1: {
+      color: "black",
+    },
+    body2: {
+      color: "#ffffff",
+    },
+  },
+});
