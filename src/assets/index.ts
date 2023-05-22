a3ecf2a4818b5057558cd74b39c9627c81affcc2
Ericles Willian <ewnsilva@gmail.com>
2023-05-22 18:25:27 -0300

    refactor: Reformulando importação de imagens

---
+++ src/assets/index.ts
@@ -0,0 +1,28 @@
+import DisplayPc from "./background/displayPc.png";
+import DisplayMobile from "./background/displayMobile.png";
+import DisplayTablet from "./background/displayTablet.png";
+
+import ClearSky from "./imgs/clearSky.png";
+import ColdRain from "./imgs/coldRain.png";
+import Fog from "./imgs/fog.png";
+import FreezingRain from "./imgs/freezingRain.png";
+import MainlyClear from "./imgs/mainlyClear.png";
+import Rain from "./imgs/rain.png";
+import Snow from "./imgs/snow.png";
+import Snowfall from "./imgs/snowfall.png";
+import Thunderstorm from "./imgs/thunderstorm.png";
+
+export {
+  DisplayMobile,
+  DisplayPc,
+  DisplayTablet,
+  ClearSky,
+  ColdRain,
+  Fog,
+  FreezingRain,
+  MainlyClear,
+  Rain,
+  Snow,
+  Snowfall,
+  Thunderstorm,
+};
