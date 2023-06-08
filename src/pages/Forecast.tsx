b0ff47a2748699f9fd95526eacc65151c607ad9a
Ericles Willian <ewnsilva@gmail.com>
2023-06-08 20:51:42 -0300

    feat: Adicionando responsividade

--- src/pages/Forecast.tsx
+++ src/pages/Forecast.tsx
@@ -1,9 +1,10 @@
 import { useCallback, useEffect, useState } from "react";
-import { Box, Typography } from "@mui/material";
+
+import { Box, Typography, useMediaQuery } from "@mui/material";
+
 import { Options, Search } from "components";
 import { useAxios } from "hooks";
-
-import { DisplayPc } from "assets";
+import { Background, BackgroundMobile } from "assets";
 
 type WheatherProps = {
   temperature: number;
@@ -17,10 +18,17 @@ type LocalizationProps = {
 
 export const Forecast = () => {
   const { api } = useAxios();
+
   const [localization, setLocalization] = useState<LocalizationProps>();
   const [wheather, setWheather] = useState<WheatherProps>();
+
   const [city, setCity] = useState("");
   const [selectedCity, setSelectedCity] = useState("");
+
+  const [loading, setLoading] = useState(false);
+
+  const matchesXs = useMediaQuery("(max-width: 465px)");
+  const matchesSm = useMediaQuery("(max-width: 650px)");
 
   const changeCity = (event: React.ChangeEvent<HTMLInputElement>) => {
     setCity(event.target.value);
@@ -53,48 +61,78 @@ export const Forecast = () => {
         })
         .then(({ data }) => {
           setWheather(data.current_weather);
+          setLoading(false);
         });
     },
     [api]
   );
 
   const onSubmit = () => {
+    setLoading(true);
     localizationSearch(city);
     setSelectedCity(city);
   };
 
   useEffect(() => {
     wheatherSearch(Number(localization?.lat), Number(localization?.lon), true);
-  }, [localization]);
+  }, [localization, wheatherSearch]);
 
   return (
     <Box
-      style={{
-        display: "grid",
-        gridTemplateColumns: "1fr",
-        rowGap: 20,
-        height: "700px",
-        width: "342px",
-        backgroundImage: `url(${DisplayPc})`,
-        textAlign: "center",
+      sx={{
+        width: "auto",
+        height: "auto",
+        maxWidth: "100%",
+        maxHeight: "100%",
+        backgroundImage: matchesXs
+          ? `url(${BackgroundMobile})`
+          : `url(${Background})`,
+        backgroundSize: "cover",
       }}
     >
-      <Box>
-        <Typography
-          variant="h2"
-          alignItems={"center"}
-          sx={{ marginTop: "20px" }}
-        >
-          Wheather App
-        </Typography>
-      </Box>
-      <Options handleChange={changeCity} handleClick={onSubmit} />
+      <Box
+        style={{
+          display: matchesXs ? " " : "flex",
+          flexDirection: "column",
+          justifyContent: matchesXs ? "center" : "",
+          alignItems: "center",
+          height: matchesXs ? "750px" : "900px",
+          width: matchesXs ? "360px" : "100%",
+          textAlign: "center",
+        }}
+      >
+        <Box display="flex" justifyContent="center">
+          <Typography
+            margin={matchesXs ? 2 : 5}
+            variant="h1"
+            alignItems="center"
+            fontFamily="Iceland"
+            fontSize={80}
+          >
+            Wheather App
+          </Typography>
+        </Box>
 
-      <Search
-        city={selectedCity}
-        temperature={wheather?.temperature}
-        wheatherStatus={wheather?.weathercode}
-      />
+        <Box display={matchesSm ? "" : "flex"}>
+          <Box
+            display="flex"
+            margin={matchesXs ? 3 : 0}
+            marginRight={matchesSm ? 0 : 3}
+            marginBottom={matchesXs ? 3 : 3}
+          >
+            <Options handleChange={changeCity} handleClick={onSubmit} />
+          </Box>
+
+          <Box display="flex" justifyContent="center">
+            <Search
+              city={selectedCity}
+              temperature={wheather?.temperature}
+              wheatherStatus={wheather?.weathercode}
+              loading={loading}
+            />
+          </Box>
+        </Box>
+      </Box>
     </Box>
   );
 };
